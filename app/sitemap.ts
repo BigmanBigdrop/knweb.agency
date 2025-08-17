018cd006c59ed2b273f8e79b114491c9703d121c
BigmanBigdrop <150232298+BigmanBigdrop@users.noreply.github.com>
2025-08-17 04:28:29 +0000

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap():  Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://www.knweb.agency";
   const currentDate = new Date().toISOString();
 
